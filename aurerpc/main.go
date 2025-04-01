4465420690ab5a7d0545936217cb1880ce158d89
Eiso Lee <1425153862@qq.com>
2025-04-02 00:18:36 +0800

    feat: add main

--- aurerpc/main.go
+++ aurerpc/main.go
@@ -1,1 +1,49 @@
-package aurerpc
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"aurerpc/codec"
+	"aurerpc/server"
+)
+
+func startServer(addr chan string) {
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	server.Accept(l)
+}
+
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	// in fact, following code is like a simple aurerpc client
+	conn, _ := net.Dial("tcp", <-addr)
+	defer func() { _ = conn.Close() }()
+
+	time.Sleep(time.Second)
+	// send options
+	_ = json.NewEncoder(conn).Encode(server.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+	// send request & receive response
+	for i := range 5 {
+		h := &codec.Header{
+			ServiceMethod: "Foo.Sum",
+			Seq:           uint64(i),
+		}
+		_ = cc.Write(h, fmt.Sprintf("aurerpc req %d", h.Seq))
+		_ = cc.ReadHeader(h)
+		var reply string
+		_ = cc.ReadBody(&reply)
+		log.Printf("aurerpc reply: %s", reply)
+	}
+}
